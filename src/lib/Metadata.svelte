fc567019e0b3a24a31797674540035f73eba9fba
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-04-11 00:36:09 +0000

    add source

---
+++ src/lib/Metadata.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let title = "Rectify11";
+	export let heroImage = "";
+	export let description = "Official Rectify11 Website";
+	export let imageHost = "";
+	export let imageProtocol = "";
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta content={title} name="og:title" />
+	{#if heroImage && imageHost && imageProtocol}
+		<meta
+			content="{imageProtocol}//{imageHost}{heroImage}"
+			name="og:image"
+		/>
+	{:else}
+	{/if}
+	<meta content={description} name="description" />
+	<meta content={description} name="og:description" />
+</svelte:head>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { Metadata } from "src/lib";
+</script>
+
+<Metadata title="Pdawg • About" />
