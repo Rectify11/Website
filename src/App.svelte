cbb5c26802c1f9c119b0d1cba4a5978a07509eb3
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-04-11 13:10:27 +0000

    add theme css local

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
   import "fluent-svelte/theme.css";
-  import { Button } from "fluent-svelte";
+  import * as fluent from "fluent-svelte";
 </script>
 
 <main>
-  <div>
-    <Button>Hi!</Button>
+  <div id="topNavbar">
+    
   </div>
 </main>
 
 <style>
-	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	@import url("/src/lib/theme.css");
 
-	/* Some base styles to get things looking right. */
 	:global(body) {
 		background-color: var(--fds-solid-background-base);
 		color: var(--fds-text-primary);
