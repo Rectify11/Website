5b58b53fc4ed12262f93b4f349fba616924896dc
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-04-11 02:17:25 +0000

    reset to svelte vanilla

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import "fluent-svelte/theme.css";
+  import { Button } from "fluent-svelte";
+</script>
+
+<main>
+  <div>
+    <Button>Hi!</Button>
+  </div>
+</main>
+
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+
+	/* Some base styles to get things looking right. */
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import { Metadata } from "src/lib";
-</script>
-
-<Metadata title="Pdawg • About" />
