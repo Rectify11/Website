f717636b877eddc1887eb2850db0d2385f4e350f
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-04-19 02:17:57 +0000

    work on ui and adaptive layout

--- src/layouts/navbar.svelte
+++ src/layouts/navbar.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-	import { Button, TextBlock } from "fluent-svelte";
+	import { Button, TextBlock, IconButton } from "fluent-svelte";
 	import '../lib/fonthelper.css';
 	import "fluent-svelte/theme.css";
 </script>
   
 <div id="topNavbar">
 	<a href="/"><img id="r11Icon" src="/rectify11_logo.png" alt="Rectify11 Logo" height="35"/></a>
+	<div id="rightMobileControl">
+		<IconButton>
+			
+		</IconButton>
+	</div>
 	<div id="rightControls">
 		<Button variant="hyperlink" height="20px" style="margin-right: 10px;" href="/community">
-			<TextBlock style="color:#B0B2B7;">Community</TextBlock>
+			<TextBlock>Community</TextBlock>
 		</Button>
 		<Button variant="hyperlink" height="20px" style="margin-right: 20px;" href="/faq">
-			<TextBlock style="color:#B0B2B7;">FAQ</TextBlock>
+			<TextBlock>FAQ</TextBlock>
 		</Button>
 		<Button variant="accent" height="20px" style="cursor: pointer;" href="/download">
 			<TextBlock style="font-family: SegoeFlnt; height: 10px; margin-top: 0px; margin-right: 4px; margin-top: -8px; margin-left: -2px; padding: 0px;">&#xE118;</TextBlock>  Download
@@ -21,7 +26,6 @@
   
   <style>
 	  :global(body) {
-		  /*background: url("https://4kwallpapers.com/images/wallpapers/windows-11-dark-background-bloom-5k-5120x2880-8399.png");*/
 		  color: var(--fds-text-primary);
 		  color-scheme: dark;
 	  }
@@ -46,6 +50,33 @@
 	  background-blend-mode: lighten;
 	  position: absolute;
 	  display: flex;
+  }
+  @media (prefers-color-scheme: light) {
+        #topNavbar
+        {
+            background-color: #a6a4a4ba;
+			background-blend-mode: darken;
+        }
+  }
+  @media (max-width: 768px) {
+		#rightControls
+		{
+			visibility: collapse;
+		}
+		#rightMobileControl
+		{
+			visibility: visible;
+		}
+  }
+  @media (min-width: 769px) {
+		#rightControls
+		{
+			visibility: visible;
+		}
+		#rightMobileControl
+		{
+			visibility: collapse;
+		}
   }
   
   #rightControls
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div id="container">
 	<div id="intro">
-        <TextBlock variant="display" style="margin-left: 10px; margin-right: 10px;">Introducing Rectify11</TextBlock>
+        <TextBlock variant="display" style="margin-left: 20px; margin-right: 20px;">Introducing Rectify11</TextBlock>
     </div>
 </div>
 
@@ -12,12 +12,20 @@
 	#container
     {
         background-color: var(--fds-solid-background-base);
-        background-image: url("./static/images/bloom_light.jpg");
         background-size: cover;
         width: 100vw;
 		height: 100vh;
 		overflow-x: hidden;
         text-align: center;
+        background-image: url("/src/lib/bloom_dark.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    @media (prefers-color-scheme: light) {
+        #container 
+        {
+            background-image: url("/src/lib/bloom_light.jpg");
+        }
     }
 
     #intro
