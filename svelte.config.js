36e68e388e174f12691b3c5d16334a63b9767d77
Misha <mishakeys20@gmail.com>
2023-08-12 10:25:34 -0400

    update webserver

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,15 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+    adapter: adapter()
+  }
 };
 export default config;
